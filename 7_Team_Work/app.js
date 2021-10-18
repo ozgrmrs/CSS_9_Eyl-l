c3128c2114428dd42db598490ce9afa017935247
ozgrmrs <ozgrmrs@gmail.com>
2021-10-19 00:12:07 +0300

    fibo ve reversed sentence eklendi

--- 7_Team_Work/app.js
+++ 7_Team_Work/app.js
@@ -1,5 +1,39 @@
-console.log("ı am from outside")
-var a = 4;
-var b = 5;
-var c = a + b;
-console.log(c);
+// console.log("ı am from outside")
+// var a = 4;
+// var b = 5;
+// var c = a + b;
+// // console.log(c);
+
+
+////////////FIBONACCI///////
+
+// let fibonacciNumber = prompt("Please enter a number:");
+//         let num1 = 0;
+//         let num2 = 1;
+//         let sum ;
+//         let fibonacciNumbers = [];
+//         for(let i = 1; i < fibonacciNumber; i++){
+//         sum = num1 + num2;
+//         num1 = num2;
+//         num2 = sum;
+//         fibonacciNumbers.push(sum);
+//  }
+// console.log(fibonacciNumbers);
+
+
+//////////REVERSE STRING////////////
+let sentence = prompt("enter a sentence or word:");
+console.log(sentence);
+function reverse (sentence1){
+    var splitString = sentence1.split("");
+    var reverseArray = splitString.reverse();
+    var joinArray = reverseArray.join("");
+    return joinArray; 
+}
+console.log(reverse(sentence));
+// reverse (sentence);
+
+
+
+
+
